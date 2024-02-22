8e4202ae53e4f1b06c2213cf7b8d2f16f028fb55
W Anders <w@doubleu.codes>
2024-02-22 01:25:24 -0700

    overhaul

--- install-k3s.sh
+++ install-k3s.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 export INSTALL_K3S_SKIP_SELINUX_RPM=true
-#export INSTALL_K3S_SKIP_DOWNLOAD=true
+
+if [ "$INSTALL_K3S_SKIP_DOWNLOAD" = true ]; then
+    mkdir -p /var/lib/rancher/k3s/agent/images/
+    cp /usr/libexec/k3s/k3s-airgap-images-* /var/lib/rancher/k3s/agent/images
+    cp /usr/libexec/k3s/k3s /usr/local/bin/k3s
+    chmod +x /usr/local/bin/k3s
+fi
 
 /usr/libexec/k3s/install.sh "$@"
